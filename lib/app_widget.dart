3caa684120b341dd828e667e37bb3568bb3e6cc1
Stenio Oliveira <steniooliv@gmail.com>
2021-07-02 23:19:26 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Beauty',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:beauty_flutter/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
