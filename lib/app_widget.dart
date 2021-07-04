8d7556b2268d2ab88d0f84cca240b93a2fe7804e
Stenio Oliveira <steniooliv@gmail.com>
2021-07-04 01:03:21 -0300

    create routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,25 @@
+import 'package:beauty_flutter/modules/create_account/create_account_page.dart';
+import 'package:beauty_flutter/modules/home/home_page.dart';
+import 'package:beauty_flutter/modules/login/login_page.dart';
+import 'package:beauty_flutter/modules/recovery_password/recovey_password.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Beauty',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.green,
       ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/create': (context) => CreateAccountPage(),
+        '/recovery': (context) => RecoveryPasswordPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
